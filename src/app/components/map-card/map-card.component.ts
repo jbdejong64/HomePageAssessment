1c1d1baed2e755a492faffd6c7ef8751a1d909b7
jbdejong64 <jacksonbdejong@gmail.com>
2021-05-13 20:32:23 -0500

    minor updates

--- src/app/components/map-card/map-card.component.ts
+++ src/app/components/map-card/map-card.component.ts
@@ -9,6 +9,7 @@ export class MapCardComponent implements OnInit {
 
   @Input() loading: boolean = true;
 
+  zoom = 13;
   startingCenter: google.maps.LatLngLiteral = {lat:20.8846913 ,lng:-156.4647848};
 
   constructor() { }
