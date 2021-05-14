b166309b6a9a44494f8183bdb8b32a662da3a28f
jbdejong64 <jacksonbdejong@gmail.com>
2021-05-13 19:37:40 -0500

    update map center

--- src/app/components/map-card/map-card.component.ts
+++ src/app/components/map-card/map-card.component.ts
@@ -9,6 +9,8 @@ export class MapCardComponent implements OnInit {
 
   @Input() loading: boolean = true;
 
+  startingCenter: google.maps.LatLngLiteral = {lat:20.8846913 ,lng:-156.4647848};
+
   constructor() { }
 
   ngOnInit(): void {
